3efa69b510e737701e4e5b00b1dcb2b13c4943ac
AbiolaM <biolamarcus@yahoo.com>
2020-09-05 22:47:11 +0100

    added scenario

--- src/test/java/stepDefinitions/BlueSkyCitadelForm.java
+++ src/test/java/stepDefinitions/BlueSkyCitadelForm.java
@@ -2,9 +2,11 @@ package stepDefinitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BlueSkyCitadelForm {
@@ -35,5 +37,25 @@ public class BlueSkyCitadelForm {
         driver.findElement(By.xpath("//*[@id=\"nf-field-135\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"nf-field-135\"]")).sendKeys("My first enter");
 
+    }
+
+    @And("^I click on Events$")
+    public void iClickOnEvents() {
+      driver.findElement(By.xpath("//*[@id=\"menu-item-704\"]/a")).click();
+    }
+
+    @And("^I click on EVENTS IN to select month$")
+    public void iClickOnEVENTSINToSelectMonth() {
+        driver.findElement(By.cssSelector("#tribe-bar-date")).sendKeys("05-09-2020");
+    }
+
+    @And("^I enter keyword in SEARCH$")
+    public void iEnterKeywordInSEARCH() {
+        driver.findElement(By.xpath("//*[@id=\"tribe-bar-search\"]")).sendKeys("Manual Training");
+    }
+
+    @Then("^I close my window$")
+    public void iCloseMyWindow() {
+        driver.close();
     }
 }
